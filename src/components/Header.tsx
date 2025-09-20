d66adf7245586ee67d55c2c1c6ce6f468b37d792
ABHISHEK THAKUR <56766419+abhishekthakur797@users.noreply.github.com>
2025-09-21 00:54:22 +0530

    Update light theme logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -254,7 +254,7 @@ const Header: React.FC<HeaderProps> = ({ user, onAuthClick }) => {
                 />
               ) : (
                 <img 
-                  src="/IHDdeals-Logo-Light-Theme.png" 
+                  src="/IHDdeals-Light-Theme.png" 
                   alt="IHD Deals Logo" 
                   className="h-12 w-auto"
                 />
